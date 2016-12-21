0bceaeb06e96979ae57b376543acd6f435ba0179
Phil <zgrubby@gmail.com>
2016-12-21 17:09:32 +0800

    test new logger

---
+++ logger_test.go
@@ -0,0 +1,33 @@
+package gol
+
+import "testing"
+
+func TestNewLogger(t *testing.T) {
+	type args struct {
+		level LogLevel
+	}
+	tests := []struct {
+		name string
+		args args
+		want Logger
+	}{
+		{
+			"case1",
+			args{
+				DEBUG,
+			},
+			&gollog{
+				level:   DEBUG,
+				option:  LstdFlags,
+				logChan: make(chan string, 1024),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLogger(tt.args.level); got.(*gollog).level != tt.args.level || got.(*gollog).option != LstdFlags || got.(*gollog).logChan == nil {
+				t.Errorf("NewLogger() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
