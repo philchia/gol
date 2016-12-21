28706e8ada4c0453009062d0ba6dfd34086c7751
Phil <zgrubby@gmail.com>
2016-12-21 15:37:05 +0800

    process prefix log

--- gollog.go
+++ gollog.go
@@ -2,6 +2,11 @@ package gol
 
 import (
 	"fmt"
+	"runtime"
+
+	"bytes"
+
+	"time"
 
 	"github.com/philchia/gol/adapter"
 	"github.com/philchia/gol/internal/stringUtil"
@@ -29,15 +34,96 @@ func (l *gollog) put(msg string) {
 	l.logChan <- msg
 }
 
-func (l *gollog) generateLog(level LogLevel, msg string) string {
-	return stringUtil.JoinStrings("[", level.String(), "] ", msg, "\n")
+// itoa: Cheap integer to fixed-width decimal ASCII.  Give a negative width to avoid zero-padding.
+func itoa(buf *bytes.Buffer, i int, wid int) {
+	// Assemble decimal in reverse order.
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || wid > 1 {
+		wid--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
+	}
+	// i < 10
+	b[bp] = byte('0' + i)
+	buf.Write(b[bp:])
+}
+
+func (l *gollog) generatePrefix(callDepth int) string {
+	var buf bytes.Buffer
+
+	///////////////////////////
+	var t = time.Now()
+	if l.option&LUTC != 0 {
+		t = t.UTC()
+	}
+	if l.option&(Ldate|Ltime|Lmicroseconds) != 0 {
+		if l.option&Ldate != 0 {
+			year, month, day := t.Date()
+			itoa(&buf, year, 4)
+			buf.WriteByte('/')
+			itoa(&buf, int(month), 2)
+			buf.WriteByte('/')
+			itoa(&buf, day, 2)
+			buf.WriteByte(' ')
+		}
+		if l.option&(Ltime|Lmicroseconds) != 0 {
+			hour, min, sec := t.Clock()
+			itoa(&buf, hour, 2)
+			buf.WriteByte(':')
+			itoa(&buf, min, 2)
+			buf.WriteByte(':')
+			itoa(&buf, sec, 2)
+			if l.option&Lmicroseconds != 0 {
+				buf.WriteByte('.')
+				itoa(&buf, t.Nanosecond()/1e3, 6)
+			}
+			buf.WriteByte(' ')
+		}
+	}
+
+	var file string
+	var line int
+	var ok bool
+	_, file, line, ok = runtime.Caller(callDepth)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+	if l.option&(Lshortfile|Llongfile) != 0 {
+		if l.option&Lshortfile != 0 {
+			short := file
+			for i := len(file) - 1; i > 0; i-- {
+				if file[i] == '/' {
+					short = file[i+1:]
+					break
+				}
+			}
+			file = short
+		}
+		buf.WriteString(file)
+		buf.WriteByte(':')
+		itoa(&buf, line, -1)
+		buf.WriteString(": ")
+	}
+
+	///////////////////////////
+
+	return buf.String()
+}
+
+func (l *gollog) generateLog(callDepth int, level LogLevel, msg string) string {
+	prefix := l.generatePrefix(callDepth)
+	return stringUtil.JoinStrings(prefix, " [", level.String(), "] ", msg, "\n")
 }
 
 func (l *gollog) Debug(i ...interface{}) {
 	if l.level > DEBUG {
 		return
 	}
-	msg := l.generateLog(DEBUG, fmt.Sprint(i...))
+	msg := l.generateLog(2, DEBUG, fmt.Sprint(i...))
 	l.put(msg)
 }
 
@@ -45,7 +131,7 @@ func (l *gollog) Debugf(format string, i ...interface{}) {
 	if l.level > DEBUG {
 		return
 	}
-	msg := l.generateLog(DEBUG, fmt.Sprintf(format, i...))
+	msg := l.generateLog(2, DEBUG, fmt.Sprintf(format, i...))
 	l.put(msg)
 }
 
@@ -53,7 +139,7 @@ func (l *gollog) Info(i ...interface{}) {
 	if l.level > INFO {
 		return
 	}
-	msg := l.generateLog(INFO, fmt.Sprint(i...))
+	msg := l.generateLog(2, INFO, fmt.Sprint(i...))
 	l.put(msg)
 }
 
@@ -61,7 +147,7 @@ func (l *gollog) Infof(format string, i ...interface{}) {
 	if l.level > INFO {
 		return
 	}
-	msg := l.generateLog(INFO, fmt.Sprintf(format, i...))
+	msg := l.generateLog(2, INFO, fmt.Sprintf(format, i...))
 	l.put(msg)
 }
 
@@ -69,7 +155,7 @@ func (l *gollog) Warn(i ...interface{}) {
 	if l.level > WARN {
 		return
 	}
-	msg := l.generateLog(WARN, fmt.Sprint(i...))
+	msg := l.generateLog(2, WARN, fmt.Sprint(i...))
 	l.put(msg)
 }
 
@@ -77,7 +163,7 @@ func (l *gollog) Warnf(format string, i ...interface{}) {
 	if l.level > WARN {
 		return
 	}
-	msg := l.generateLog(WARN, fmt.Sprintf(format, i...))
+	msg := l.generateLog(2, WARN, fmt.Sprintf(format, i...))
 	l.put(msg)
 }
 
@@ -85,7 +171,7 @@ func (l *gollog) Error(i ...interface{}) {
 	if l.level > ERROR {
 		return
 	}
-	msg := l.generateLog(ERROR, fmt.Sprint(i...))
+	msg := l.generateLog(2, ERROR, fmt.Sprint(i...))
 	l.put(msg)
 }
 
@@ -93,7 +179,7 @@ func (l *gollog) Errorf(format string, i ...interface{}) {
 	if l.level > ERROR {
 		return
 	}
-	msg := l.generateLog(ERROR, fmt.Sprintf(format, i...))
+	msg := l.generateLog(2, ERROR, fmt.Sprintf(format, i...))
 	l.put(msg)
 }
 
@@ -101,7 +187,7 @@ func (l *gollog) Critical(i ...interface{}) {
 	if l.level > CRITICAL {
 		return
 	}
-	msg := l.generateLog(CRITICAL, fmt.Sprint(i...))
+	msg := l.generateLog(2, CRITICAL, fmt.Sprint(i...))
 	l.put(msg)
 }
 
@@ -109,7 +195,7 @@ func (l *gollog) Criticalf(format string, i ...interface{}) {
 	if l.level > CRITICAL {
 		return
 	}
-	msg := l.generateLog(CRITICAL, fmt.Sprintf(format, i...))
+	msg := l.generateLog(2, CRITICAL, fmt.Sprintf(format, i...))
 	l.put(msg)
 }
 
