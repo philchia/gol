588c7681d0939306ce0c2bcc4f23aacc579a59a0
Phil <zgrubby@gmail.com>
2016-12-22 15:13:02 +0800

    update rotate file adapter

--- adapter/rotatefile/adapter.go
+++ adapter/rotatefile/adapter.go
@@ -1,4 +1,4 @@
-package file
+package rotatefile
 
 import (
 	"io"
---
+++ adapter/rotatefile/adapter_test.go
@@ -0,0 +1,97 @@
+package rotatefile
+
+import (
+	"io"
+	"reflect"
+	"testing"
+
+	"errors"
+
+	"github.com/philchia/gol/adapter"
+)
+
+type fakeWriter struct {
+	withErr error
+}
+
+func (w *fakeWriter) Write(b []byte) (int, error) {
+	if w.withErr != nil {
+		return 0, w.withErr
+	}
+	return len(b), nil
+}
+
+func (w *fakeWriter) Close() error {
+	return nil
+}
+
+func TestNewFileAdapter(t *testing.T) {
+	type args struct {
+		pathToFile string
+	}
+	tests := []struct {
+		name string
+		args args
+		want adapter.Adapter
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewAdapter(tt.args.pathToFile, 1, MB); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFileAdapter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fileAdapter_Write(t *testing.T) {
+	type fields struct {
+		writer io.WriteCloser
+	}
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"case1",
+			fields{
+				new(fakeWriter),
+			},
+			args{
+				[]byte("hi"),
+			},
+			false,
+		},
+		{
+			"case2",
+			fields{
+				&fakeWriter{
+					withErr: errors.New("test"),
+				},
+			},
+			args{
+				[]byte("hi"),
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &rotatefileAdapter{
+				WriteCloser:        tt.fields.writer,
+				maxFileNum:         3,
+				maxByteSizePerFile: MB,
+				fileName:           "tem.log",
+			}
+			if _, err := a.Write(tt.args.b); (err != nil) != tt.wantErr {
+				t.Errorf("fileAdapter.Write() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
